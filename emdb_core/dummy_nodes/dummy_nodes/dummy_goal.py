7e4b5adfd5975ee1ab56b7a599e153faf9a3501a
ines <inestianrp@gmail.com>
2026-07-24 11:50:05 +0200

    added drive and purpose nodes for goals nodes created during alignment, changed activations values for llm planner to be higher than alignment

--- emdb_core/dummy_nodes/dummy_nodes/dummy_goal.py
+++ emdb_core/dummy_nodes/dummy_nodes/dummy_goal.py
@@ -3,21 +3,7 @@ from cognitive_nodes.goal import GoalMotiven
 class GoalDummy(GoalMotiven):
     """
     Dummy Goal class: A goal that is never rewarded.
-    This Goal has the activation value manually given thought /set_activation service.
     """
-
-    def calculate_activation(self, perception = None, activation_list=None):
-        """
-        Returns the the activation value of the goal
-
-        :param perception: Perception does not influence the activation 
-        :type perception: dict
-        :return: The activation of the goal
-        :rtype: float
-        """
-        if self.activation_topic:
-            self.publish_activation(self.activation)
-        return self.activation
 
     def calculate_reward(self, drive_name): #No reward is provided
         """
